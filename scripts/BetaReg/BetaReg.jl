efcf6640aa815e4f7565c7796002fa65435522d6
Mattias Villani <mattias.villani@gmail.com>
2026-01-23 14:56:53 +0100

    just some little changes to the BetaReg example

--- scripts/BetaReg/BetaReg.jl
+++ scripts/BetaReg/BetaReg.jl
@@ -20,7 +20,7 @@ BetaMean(μ, ψ) = Beta(eps() + μ*ψ, eps() + (1-μ)*ψ)
 
 # ### Simulate data from the Beta regression model with fixed parameter paths
 T = 500;
-p = 3; # Number of covariate in μ, including intercept
+p = 1; # Number of covariate in μ, including intercept
 q = 1; # Number of covariates in ψ, including intercept
 Xmean = [ones(T+1) randn(T+1, p-1)]; # Design matrix
 Xprec = [ones(T+1) randn(T+1, q-1)];                 # Design matrix for precision
@@ -28,7 +28,7 @@ y = zeros(Float64, T+1)
 β = zeros(T+1,p) # Store the regression parameters
 γ = zeros(T+1,q) # Store the precision parameters
 β[1,:] = [0.0, 0.0, 0.5]
-γ[1,:] = [log(5)] # log precision intercept
+γ[1,:] = [1] # log precision intercept
 μtime = zeros(T+1)
 ψtime = zeros(T+1)
 invlinkmean_dgp(x) = logistic(x) # Inverse link function for Beta regression
@@ -45,7 +45,8 @@ for t in 2:(T+1)
         end
     end
     β[t,3] = 0.5
-    γ[t,1] = t/T < 0.4 ? log(5) : log(50 - 40*(t/T))
+    γ[t,1] = 1
+    #γ[t,1] = t/T < 0.4 ? log(5) : log(50 - 40*(t/T))
     μtime[t] = invlinkmean_dgp(Xmean[t,:]⋅β[t,:])
     ψtime[t] = invlinkprec_dgp(Xprec[t,:]⋅γ[t,:])
     y[t] = rand(BetaMean(μtime[t], ψtime[t]))
@@ -55,6 +56,21 @@ Xmean = Xmean[2:end,:];
 Xprec = Xprec[2:end,:];
 μtime = μtime[2:end];
 ψtime = ψtime[2:end];
+αtime = μtime .* ψtime
+βtime = (1 .- μtime) .* ψtime
+
+
+# plot \mu and \psi time series
+p1 = plot(μtime, xlabel = "time, "*L"t", title = L"\mu_t", lw = 2,  
+    color = colors[1], legend = nothing)
+p2 = plot(ψtime, xlabel = "time, "*L"t", title = L"\psi_t", lw = 2,  
+    color = colors[3], legend = nothing)    
+p3 = plot(αtime, xlabel = "time, "*L"t", title = L"\alpha_t", lw = 2,  
+    color = colors[2], legend = nothing)
+p4 = plot(βtime, xlabel = "time, "*L"t", title = L"\beta_t", lw = 2,  
+    color = colors[4], legend = nothing)    
+plot(p1, p2, p3, p4, layout = (4,1), size = (1000,1200), 
+    xguidefontsize = 12, yguidefontsize = 14, titlefontsize=18, margin = 5mm)
 
 # ### Plot the parameter evolution path of βₜ
 plt = []
@@ -98,9 +114,9 @@ plot(y, xlabel = "time, "*L"t", ylabel = L"y_t", lw = 1,
 # ### Set up the prior, model and algorithm settings
 priorSettings = (
     ϕ₀ = 0.5, κ₀ = 0.3,         # Prior for ϕ ~ N(ϕ₀, κ₀²)
-    m₀ = -15.0, σ₀ = 3.0,       # Prior for μ ~ N(m₀, σ₀²)
+    m₀ = -8.0, σ₀ = 3.0,       # Prior for μ ~ N(m₀, σ₀²)
     ν₀ = 3.0, ψ₀ = 1.0,         # Prior for σ²ₙ ~ scaled inverse χ²(ν₀, ψ₀)
-    μ₀ = zeros(p+q), Σ₀ = 10*I(p+q),# Prior for βₜ at time t=0
+    μ₀ = zeros(p+q), Σ₀ = 1*I(p+q),# Prior for βₜ at time t=0
 ); 
 
 # ### Set up the Poisson regression model
@@ -112,8 +128,6 @@ end
 
 invlinkmean(x) = logistic(x) # inverse link function for μ in Beta regression
 invlinkprecision(x) = exp(x) # inverse link function for ψ in Beta regression
-const p = size(Xmean,2)
-const q = 1 # Number of precision covariates
 observation(param, state, t) = 
     product_distribution(
         BetaMean.(
@@ -175,7 +189,6 @@ PlotPostParamEvolution!(plt, PGAS_quantiles, "PGAS($(algoSettings.nParticles))";
     dateVec = nothing, interval_style = :shaded, lw = 2, c = :gray)
 plot(plt..., layout = (3,1), size = (1400, 1000), xlabel = "time", 
     bottommargin = 5mm, ylims = [-1.5,1.5], legend = :bottomleft)
-
 
 
 # ### Laplace approximation
