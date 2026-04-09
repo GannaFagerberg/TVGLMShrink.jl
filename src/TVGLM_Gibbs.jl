70521776c1f670e1c8779a3aadb8d6d400879c8e
YijieNiu <your_email@example.com>
2026-04-09 17:00:31 +0200

    Output original state

--- src/TVGLM_Gibbs.jl
+++ src/TVGLM_Gibbs.jl
@@ -105,8 +105,9 @@ function GibbsTVGLM(Y, priorSettings, modelSettings, algoSettings)
             error("Only :ffbs_laplace or :pgas are implemented yet.")
         end
 
+        θ = θ * inv(scaling) 
         ## Update the log-volatility evolution
-        ν = diff(θ, dims = 1) * inv(scaling)
+        ν = diff(θ, dims = 1) #* inv(scaling)
         
         setOffset!(offset, ν, offsetMethod)
 
